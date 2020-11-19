658b675fe114b673c53a6ff583daa0b58c9e749b
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2020-11-18 22:59:36 -0600

    Usando provider para optimizar codigo

--- custom_painter/lib/src/widgets/pinterest_menu.dart
+++ custom_painter/lib/src/widgets/pinterest_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 // Clase personalizada para recibir solo este tipo de dato
 class PinterestButton {
@@ -39,9 +40,96 @@ class PinterestMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        child: Text('menu'),
+      child: ChangeNotifierProvider(
+        create: (_) => new _MenuModel(),
+        child: _PinterestMenuBackground(
+          child: _MenuItems(items),
+        ),
       ),
     );
+  }
+}
+
+// Este widget sirve para los bordes y los colores
+class _PinterestMenuBackground extends StatelessWidget {
+  //Recibir los hijos
+  final Widget child;
+
+  _PinterestMenuBackground({@required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+      width: 250,
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(100)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black38,
+            blurRadius: 10,
+            spreadRadius: -5,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//Sirve para crear los items
+class _MenuItems extends StatelessWidget {
+  final List<PinterestButton> menuItems;
+  _MenuItems(this.menuItems);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(
+        menuItems.length,
+        (index) => _PinterestMenuButton(index, menuItems[index]),
+      ),
+    );
+  }
+}
+
+//Se encarga de cada uno de los botones
+class _PinterestMenuButton extends StatelessWidget {
+  final int index;
+  final PinterestButton item;
+
+  _PinterestMenuButton(this.index, this.item);
+
+  @override
+  Widget build(BuildContext context) {
+    final itemSeleccionado = Provider.of<_MenuModel>(context).itemSeleccionado;
+    return GestureDetector(
+      onTap: () {
+        Provider.of<_MenuModel>(context, listen: false).itemSeleccionado =
+            index;
+        item.onPressed();
+      },
+      behavior: HitTestBehavior.translucent,
+      child: Container(
+        child: Icon(
+          item.icon,
+          size: (itemSeleccionado == index) ? 35 : 20,
+          color: (itemSeleccionado == index) ? Colors.black : Colors.blueGrey,
+        ),
+      ),
+    );
+  }
+}
+
+//Se usa para compartir el item seleccionado usando provider
+class _MenuModel with ChangeNotifier {
+  int _itemSeleccionado = 0;
+
+  int get itemSeleccionado => this._itemSeleccionado;
+
+  set itemSeleccionado(int index) {
+    this._itemSeleccionado = index;
+    notifyListeners();
   }
 }
