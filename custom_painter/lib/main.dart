98b3edd5c6f059e1cdabf1749d8e0b475cf5e068
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2020-11-18 21:49:09 -0600

    Estilo de pinterest, diseño basico del menu

--- custom_painter/lib/main.dart
+++ custom_painter/lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:custom_painter/src/pages/pinterest_page.dart';
 import 'package:flutter/material.dart';
 
-import 'package:custom_painter/src/pages/slideshow_page.dart';
-
+//import 'package:custom_painter/src/pages/slideshow_page.dart';
 //import 'package:custom_painter/src/labs/slideshow_page.dart';
 //import 'package:custom_painter/src/pages/graficas_circulares_page.dart';
 //import 'package:custom_painter/src/labs/circular_progress_page.dart';
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños App',
-      home: SlideshowPage(),
+      home: PinterestPage(),
     );
   }
 }
